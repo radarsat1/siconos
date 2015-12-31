28cda3c0d1e532b3004e9326a2aeed858a42c792
Stephen Sinclair <radarsat1@gmail.com>
2015-12-31 11:25:29 -0300

    My options for compiling Siconos.

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 2)
+         (fill-column . 80)))
+ (c-mode . ((c-file-style . "k&r")
+						(c-basic-offset . 2)))
+ (c++-mode . ((c-file-style . "k&r")
+							(c-basic-offset . 2)
+              ;; (compile-history . (
+                                ;; "ninja -C ~/projects/gazebo/bld gazebo/physics/UNIT_PhysicsEngine_TEST && ~/projects/gazebo/bld/gazebo/physics/UNIT_PhysicsEngine_TEST --gtest_filter=PhysicsEngines/PhysicsEngineTest.PhysicsEngineParam/4"
+                                ;; "ninja -C ~/projects/gazebo/bld gazebo/physics/UNIT_PhysicsEngine_TEST && gazebo/physics/UNIT_PhysicsEngine_TEST" "ninja -C ~/projects/gazebo/bld gazebo/physics/UNIT_PhysicsEngine_TEST && ninja -C ~/projects/gazebo/bld install"
+                                ;; "ninja -C ~/projects/gazebo/bld gazebo/physics/UNIT_PhysicsEngine_TEST"
+                                ;; "ninja -C ~/projects/gazebo/bld"))
+              ))
+)
